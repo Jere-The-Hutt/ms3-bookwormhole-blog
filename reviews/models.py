e2ece3f9cf1fecf62db002266286c880dfe0d87c
Jere-The-Hutt <jere.huttunen@live.com>
2025-05-20 17:57:48 +0200

    Fix author field in Post model

--- reviews/models.py
+++ reviews/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=250)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
+        User, on_delete=models.CASCADE, related_name="reviews_posts"
         )
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
