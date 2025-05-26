d93d4768e54d31b666a1aadec18dfd20026f6014
Jere-The-Hutt <jere.huttunen@live.com>
2025-05-26 16:21:57 +0200

    Add Genre model and link to Post model

--- reviews/models.py
+++ reviews/models.py
@@ -5,8 +5,22 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    genre = models.ForeignKey(
+        Genre, on_delete=models.SET_NULL, null=True, related_name='posts'
+        )
     slug = models.SlugField(max_length=250)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="reviews_posts"
