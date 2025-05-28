33a97311d968fe7421bea10554c3313dc116d277
Jere-The-Hutt <jere.huttunen@live.com>
2025-05-28 11:37:29 +0200

    Improve account templates layout, add style to genre_posts.html and remove __pycache__ from tracking

--- reviews/views.py
+++ reviews/views.py
@@ -8,7 +8,7 @@ from .forms import CommentForm
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1)
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "reviews/index.html"
     paginate_by = 6
 
